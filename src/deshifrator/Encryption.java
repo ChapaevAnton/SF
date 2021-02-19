3e46ad7d1c7259d2f77a210a77daa4a893255ce8
ChapaevAnton <virsus2009@gmail.com>
2021-02-19 12:55:36 +0700

    add 19.02.21
    * two new overload methods encrypt and deEncrypt
    * left to develop atbashEncrypting

--- src/deshifrator/Encryption.java
+++ src/deshifrator/Encryption.java
@@ -5,7 +5,11 @@ public final class Encryption {
     public final static int XOR_ENCRYPT = 1;
     public final static int ATBASH_ENCRYPT = 2;
 
+    private final static int CODING_ALGORITHM_MODE = 1;
+    private final static int DECODING_ALGORITHM_MODE = 2;
+
     public static String encrypt(String text, String encryptKey) {
+
         return xorEncrypting(text, encryptKey);
     }
 
@@ -16,8 +20,26 @@ public final class Encryption {
 
     public static String encrypt(String text, String encryptKey, int typeCryptMethod) {
 
-        return text;
+        String newText = switch (typeCryptMethod) {
+            case XOR_ENCRYPT -> xorEncrypting(text, encryptKey);
+            case ATBASH_ENCRYPT -> atbashEncrypting(text, encryptKey, CODING_ALGORITHM_MODE);
+            default -> text;
+        };
+
+        return newText;
     }
+
+
+    public static String deEncrypt(String text, String encryptKey, int typeCryptMethod) {
+        String newText = switch (typeCryptMethod) {
+            case XOR_ENCRYPT -> xorEncrypting(text, encryptKey);
+            case ATBASH_ENCRYPT -> atbashEncrypting(text, encryptKey, DECODING_ALGORITHM_MODE);
+            default -> text;
+        };
+
+        return newText;
+    }
+
 
     private static String xorEncrypting(String text, String encryptKey) {
         char[] texts = text.toCharArray();
@@ -31,6 +53,12 @@ public final class Encryption {
         for (char item : texts) {
             text = text.replace(item, (char) (item ^ key));
         }
+
+        return text;
+    }
+
+    //TODO Разработать
+    private static String atbashEncrypting(String text, String encryptKey, int algorithmMode) {
 
         return text;
     }
