56de8f1241fa332db9d38411724433e2278cd045
Chapaev Anton <virsus2009@gmail.com>
2021-03-05 20:35:34 +0700

    up 05.03.2021
    *добавил Unit тесты - возможно кривые - это мои первые тесты
    **дорабатывай на свое усмотрение...

--- src/test/java/EncryptStringTest.java
+++ src/test/java/EncryptStringTest.java
@@ -1,21 +1,56 @@
 import deshifrator.CreatingString;
 import deshifrator.EncryptingString;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class EncryptStringTest {
 
-    @Test
-    void toStringTest() {
-        String strIn = " !\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~«»АБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯабвгдежзийклмнопрстуфхцчшщъыьэюя–";
-        EncryptingString atbashString = CreatingString.atbashString.toCreate(strIn);
-        EncryptingString XoRString = CreatingString.XoRString.toCreate(strIn);
-        EncryptingString enigmaString = CreatingString.enigmaString.toCreate(strIn);
+    static String strIn;
+    static String strOut;
+    EncryptingString atbashString;
+    EncryptingString XoRString;
+    EncryptingString enigmaString;
 
-        assertEquals(strIn, atbashString.toString(), "Error test toStringTest() - atbashString");
-        assertEquals(strIn, XoRString.toString(), "Error test toStringTest() - XoRStringString");
-        assertEquals(strIn, enigmaString.toString(), "Error test toStringTest() - enigmaString");
+    @Test
+    @BeforeAll
+    static void variableInitialization() {
+        strIn = " !\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~«»АБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯабвгдежзийклмнопрстуфхцчшщъыьэюя–";
+    }
+
+    @Test
+    @DisplayName("Проверка создания шифрованной строки")
+    @BeforeEach
+    void verifyingTheCreationOfEncryptedStrings() {
+
+        atbashString = CreatingString.atbashString.toCreate(strIn);
+        XoRString = CreatingString.XoRString.toCreate(strIn);
+        enigmaString = CreatingString.enigmaString.toCreate(strIn);
+        assertNotNull(atbashString,"Error test notNull - atbashString");
+        assertNotNull(XoRString,"Error test notNull - XoRStringString");
+        assertNotNull(enigmaString,"Error test notNull - enigmaString");
+    }
+
+
+    @Test
+    @DisplayName("Тест перевода шифрованной строки EncryptingString в String")
+    @Order(1)
+    void translateEncryptedStringToString() {
+
+        assertEquals(strIn, atbashString.toString(), "Error test toString() - atbashString");
+        assertEquals(strIn, XoRString.toString(), "Error test toString() - XoRStringString");
+        assertEquals(strIn, enigmaString.toString(), "Error test toString() - enigmaString");
+    }
+
+    @Test
+    @DisplayName("Тест шифрования/дешифрования алгоритм XoR")
+    @Order(2)
+    void encryptionAlgorithmCheckXorString() {
+        XoRString.encrypt(strIn);
+        XoRString.deEncrypt(strIn);
+        strOut = XoRString.toString();
+        assertEquals(strIn, strOut, "Error test encrypt() deEncrypt() - XoRString");
     }
 
 }
