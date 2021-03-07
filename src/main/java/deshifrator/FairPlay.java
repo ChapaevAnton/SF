0ed36cf2dbf0589caaafd36568259f8065693aa9
Ivan <grafluchivan@gmail.com>
2021-03-07 13:45:03 +0200

    07.03.21

--- src/main/java/deshifrator/FairPlay.java
+++ src/main/java/deshifrator/FairPlay.java
@@ -27,15 +27,30 @@ public class FairPlay {
         char[] key = keys.toCharArray();
 
         String alphabets = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-
-
         alphabets = keys + (alphabets.replaceAll("[" + keys + "]", ""));
-
 
         System.out.println(Arrays.toString(key));
         System.out.println(alphabets);
 
+        char[] tempAlphabet = alphabets.toCharArray();
+        char[][] newAlphabet = new char[5][5];
+        for (int i = 0; i < newAlphabet.length; i++) {
+            for (int j = 0; j < newAlphabet[i].length; j++) {
+                newAlphabet[i][j] = tempAlphabet[i * newAlphabet[j].length + j];
+            }
+        }
+//        System.out.println(Arrays.deepToString(newAlphabet));
 
+        text = text.replaceAll(" ", "");
+        char[][] bigramm = new char[text.length() / 2][2];
+//        System.out.println(text);
+        char[] arrayText = text.toCharArray();
+        for (int i = 0; i < bigramm.length; i++) {
+            for (int j = 0; j < bigramm[i].length; j++) {
+                bigramm[i][j] = arrayText[i * bigramm[j].length + j];
+            }
+        }
+        System.out.println(Arrays.deepToString((bigramm)));
     }
 
 
